e7e84bd28761b37da142d57967392b913127573e
Serhat <demircioluserhat@gmail.com>
2024-03-19 17:31:49 +0300

    Restaurant Order App UI in React Native
    Restaurant Order App UI in React Native

---
+++ constants/index.js
@@ -0,0 +1,41 @@
+export const categories = [
+  "Pizza",
+  "Tatlı",
+  "Burger",
+  "Ev Yemekleri",
+  "Kahvaltı",
+  "Kahve",
+  "Uzak Doğu"
+]
+
+export const foodItems = [
+  {
+    name: ' Pizza',
+    price: '249,99',
+    ingredients: 'sucuk, mantar, kaşar',
+    desc: 'İtalyan mutfağının eşsiz lezzetlerinden ilham alan enfes pizzalarımızı deneyin! Taptaze malzemelerle hazırlanan ince hamurumuz, zengin soslar ve çeşitli peynirlerle buluşuyor. Her bir diliminde damakları şenlendiren bir lezzet serüveni sunan pizzalarımız, özenle seçilmiş ve özel olarak hazırlanmış malzemelerle doludur. İster klasik favorilerimizi tercih edin, ister yaratıcı özel tariflerimizi deneyin.',
+    image: require('../assets/images/pizza.png'), 
+  },
+  {
+    name: 'Hamburger',
+    price: '179,99',
+    ingredients: 'Tavuk, Burger Sos, avocado',
+    desc: 'Tazelik ve lezzetin buluştuğu hamburgerlerimiz, her ısırıkta muhteşem bir tat deneyimi sunar. Seçkin malzemelerle hazırlanan etimiz, yumuşacık ekmeğimiz ve çeşitli lezzetli soslarımızla, size doyumsuz bir ziyafet vaat ediyoruz. Klasik favorilerimizi veya özel tariflerimizi deneyerek, burger tutkunlarını memnun edecek bir yolculuğa çıkın! ',
+    image: require('../assets/images/burger.png'),
+  },
+  {
+    name: 'Tatlı',
+    price: '20.00',
+    ingredients: 'Lotus Bisküvisi, Belçika Çikolatası',
+    desc: "The rice is rolled into bite-sized pieces, with a thin sheet of nori seaweed wrapped around it. Inside, you'll find a range of delicacies such as salmon, tuna, crab, avocado, cucumber, or shrimp. The sushi is served with soy sauce, wasabi, and pickled ginger to enhance the flavors. Each piece of sushi is a work of art.",
+    image: require('../assets/images/sushi.png'),
+  },
+  {
+    name: 'Kahve Çeşitleri',
+    price: '10.50',
+    ingredients: 'Yıkanmış Colombia, Karamel Özü',
+    desc: 'Ramen is a popular Japanese dish that consists of chewy noodles in a savory broth, topped with a range of flavorful ingredients. The broth can be made with pork, chicken, or vegetable stock, flavored with soy sauce, miso, or other seasonings. The noodles are cooked until they are tender and then added to the broth along with toppings such as slices of pork belly.',
+    image: require('../assets/images/ramen.png'),
+  },
+
+]
